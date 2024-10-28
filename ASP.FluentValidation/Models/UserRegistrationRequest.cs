36a0edc93d4d5d1438f8a1fb44317a151fde00d0
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-10-28 19:42:56 -0400

    Add project files.

---
+++ ASP.FluentValidation/Models/UserRegistrationRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace ASP.FluentValidation.Models
+{
+    public class UserRegistrationRequest
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+    }
+}
